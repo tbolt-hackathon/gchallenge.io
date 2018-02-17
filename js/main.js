545231903b5b8dff015ed004a2c64a5bf74f5070
tkashkin <tkashkin@gmail.com>
2018-02-18 01:09:25 +0300

    Load posts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 (function () {
+	//var APPTAG = 'gchallenge';
+	var APPTAG = 'ru--blokcheijn';
 	var config = null, node = null;
+	var templates = {};
 
-	function loadConfig() {
+	function loadConfig(callback){
 		$.ajax({
 			url: '_config/config.json',
 			dataType: 'json'
@@ -19,14 +22,64 @@
 						golos.config.set('websocket', node.address);
 						golos.config.set('chain_id', node.chainid);
 						$('body > header > nav > a.golos-io').attr('href', node.webclient);
+
+						if(callback != null) callback();
 					}
 				});
 			}
 		});
 	}
 
+	function loadTemplates(callback){
+		$.ajax({
+			url: 'views/article.htm',
+			dataType: 'text'
+		}).done(function(tpl){
+			templates.article = tpl;
+			if(callback && templates.article && templates.article_video) callback();
+		});
+		$.ajax({
+			url: 'views/article_video.htm',
+			dataType: 'text'
+		}).done(function(tpl){
+			templates.article_video = tpl;
+			if(callback && templates.article && templates.article_video) callback();
+		});
+	}
+	
+	function post() {
+		
+	}
+
+	function loadPosts(){
+		if(!templates || !templates.article) loadTemplates(loadPosts);
+
+		golos.api.getDiscussionsByTrending({select_tags: [APPTAG], limit: 100}, function(err, result){
+			if(!err && result)
+			{
+				for(var k in result)
+				{
+					var post = result[k];
+
+					var view = $(templates.article);
+					view.find('.title').text(post.title);
+					view.find('.author').text(post.author);
+					view.find('.body').html(post.body);
+
+					$('div.posts').append(view);
+				}
+			}
+		});
+	}
+
 	$(function(){
-		loadConfig();
+		loadConfig(function(){
+			loadPosts();
+		});
+
+		$('a.action-signin').on('click', function(){
+			$('section.signin, button.signin-signin').toggleClass('hidden');
+		});
 	});
 
 })();
